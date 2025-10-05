69950ffa984ec854d4f332eaf9a22354f1806e54
hyeseon-han <hyesh45@naver.com>
2025-10-05 18:30:01 +0900

    [level 2] Title: n^2 배열 자르기, Time: 7.31 ms, Memory: 44.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -0,0 +1,11 @@
+function solution(n, left, right) {
+    const answer = [];
+    
+    for(let i = left; i <= right; i++){
+        const row = Math.floor(i/n) + 1;
+        const col = i%n + 1;
+        answer.push(Math.max(row,col));
+    }
+    
+    return answer;
+}
