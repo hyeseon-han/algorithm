089c590fc3bd200b6a59386c5441324fa095e0f2
hyeseon-han <hyesh45@naver.com>
2025-11-22 23:11:11 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    while(str1.length > str2.length){
+        
+    }
+};
