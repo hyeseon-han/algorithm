981f929d1e16c09fcb40683470dc2f87105efb30
hyeseon-han <hyesh45@naver.com>
2026-02-24 23:04:14 +0900

    Time: 0 ms (100%), Space: 57.3 MB (19.4%) - LeetHub

--- LeetCode/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ LeetCode/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,14 +3,5 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    const arr = s.split(' ');
-    console.log(arr);
-    let answer = '';
-    arr.forEach(ele => answer += ele.trim());
-    for(let i = 0; i < arr.length; i++){
-        if(i > 0){
-            answer += ' ';
-        }
-        answer += arr[i].trim();
-    }
+    return s.split(' ').reverse().filter(ele => ele !== '').join(' ');
 };
